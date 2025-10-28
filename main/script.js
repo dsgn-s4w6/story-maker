f370a88361d533ef13d1aee5dd557b5a01a30fe3
design.s4w6 <design.s4w6@gmail.com>
2025-10-28 20:28:02 +0000

    Started working on the upload picture feature, ongoing

--- main/script.js
+++ main/script.js
@@ -1,11 +1,13 @@
-var titleField = document.getElementById("titleField");
-var directorField = document.getElementById("directorField");
-var actor1Field = document.getElementById("actor1Field");
-var actor2Field = document.getElementById("actor2Field");
-var titleOutput = document.getElementById("title");
-var directorOutput = document.getElementById("director");
-var actor1Output = document.getElementById("actor1");
-var actor2Output = document.getElementById("actor2");
+const imageUpload = document.getElementById("imageUpload");
+const titleField = document.getElementById("titleField");
+const directorField = document.getElementById("directorField");
+const actor1Field = document.getElementById("actor1Field");
+const actor2Field = document.getElementById("actor2Field");
+const imageOutput = document.getElementById("image");
+const titleOutput = document.getElementById("title");
+const directorOutput = document.getElementById("director");
+const actor1Output = document.getElementById("actor1");
+const actor2Output = document.getElementById("actor2");
 
 function refreshOutputs(field, output){
     output.innerHTML = field.value;
@@ -17,7 +19,18 @@ function refreshOutputs(field, output){
     }
 }
 
+function displayImage(input){
+    if(input.files && input.files[0]){
+        var reader = new FileReader();
+        reader.onload = function(e){
+            imageOutput.style.background = "url(\"e.target.result\")";
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
 titleField.addEventListener("input", function(){refreshOutputs(titleField, titleOutput)});
 directorField.addEventListener("input", function(){refreshOutputs(directorField, directorOutput)});
 actor1Field.addEventListener("input", function(){refreshOutputs(actor1Field, actor1Output)});
 actor2Field.addEventListener("input", function(){refreshOutputs(actor2Field, actor2Output)});
+imageUpload.addEventListener("upload", function(){displayImage(this)});
