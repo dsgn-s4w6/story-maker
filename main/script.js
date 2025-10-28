75197c0ad74b328c88257f0a63dad531f1403b52
design.s4w6 <design.s4w6@gmail.com>
2025-10-28 16:56:04 +0000

    Added files and text input processing. Ongoing.

---
+++ main/script.js
@@ -0,0 +1,18 @@
+var titleField = document.getElementById("titleField");
+var directorField = document.getElementById("directorField");
+var actor1Field = document.getElementById("actor1Field");
+var actor2Field = document.getElementById("actor2Field");
+
+var titleOutput = document.getElementById("result");
+var directorOutput = document.getElementById("director");
+var actor1Output = document.getElementById("actor1");
+var actor2Output = document.getElementById("actor2");
+
+titleField.addEventListener("input", function(){titleOutput.innerHTML=titleField.value;console.log("refreshed title")
+});
+directorField.addEventListener("input", function(){directorOutput.appendChild=directorField.value;console.log("refreshed director")
+});
+actor1Field.addEventListener("input", function(){actor1Output.innerHTML=actor1Field.value
+});
+actor2Field.addEventListener("input", function(){actor2Output.innerHTML=actor2Field.value
+});
