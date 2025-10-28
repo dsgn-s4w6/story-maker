3be6e1ed8cdad414d4fafa708cd6cad963e1ef89
design.s4w6 <design.s4w6@gmail.com>
2025-10-28 19:15:51 +0000

    Finished hiding output tags when empty

--- main/script.js
+++ main/script.js
@@ -2,21 +2,22 @@ var titleField = document.getElementById("titleField");
 var directorField = document.getElementById("directorField");
 var actor1Field = document.getElementById("actor1Field");
 var actor2Field = document.getElementById("actor2Field");
-
 var titleOutput = document.getElementById("title");
 var directorOutput = document.getElementById("director");
 var actor1Output = document.getElementById("actor1");
 var actor2Output = document.getElementById("actor2");
 
-titleField.addEventListener("input", refreshOutputs(titleField,titleOutput));
-directorField.addEventListener("input", function(){directorOutput.innerHTML=directorField.value;console.log("refreshed director")
-});
-actor1Field.addEventListener("input", function(){actor1Output.innerHTML=actor1Field.value
-});
-actor2Field.addEventListener("input", function(){actor2Output.innerHTML=actor2Field.value
-});
-
 function refreshOutputs(field, output){
-    output.innerHTML = field.value
-    output.style.display = "block"
+    output.innerHTML = field.value;
+    if(field.value == ""){
+        output.parentNode.style.display = "none";
+    }
+    else{
+        output.parentNode.style.display = "block";
+    }
 }
+
+titleField.addEventListener("input", function(){refreshOutputs(titleField, titleOutput)});
+directorField.addEventListener("input", function(){refreshOutputs(directorField, directorOutput)});
+actor1Field.addEventListener("input", function(){refreshOutputs(actor1Field, actor1Output)});
+actor2Field.addEventListener("input", function(){refreshOutputs(actor2Field, actor2Output)});
